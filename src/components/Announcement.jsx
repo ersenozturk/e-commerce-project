1e61554ed6ac8c88ebc2b6783ed07440286f807e
ersen <70ozturkersen@gmail.com>
2022-05-19 14:47:11 +0300

    annoucement component done

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled'
+import React from 'react'
+
+const Container= styled.div`
+  height: 30px;
+  background-color: teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+  font-size: 14px;
+  color: #fff;
+  font-weight: 500;
+`
+
+const Announcement = () => {
+  return (
+    <Container>
+    Super Deal! Free Shipping on Orders Over $50
+    </Container>
+  )
+}
+
+export default Announcement
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Annoucement from '../components/Announcement'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Annoucement />
+
+    </div>
+  )
+}
+
+export default Home
