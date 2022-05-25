2cc8cb3bd53e02099c9238c3ac324182bb3d4bca
ersen <70ozturkersen@gmail.com>
2022-05-25 13:54:46 +0300

    img url chanced for id:1

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://i.ibb.co/XsdmR2c/1.png",
+    img: "https://st2.depositphotos.com/4720955/8292/i/450/depositphotos_82922016-stock-photo-model-pose-on-white-background.jpg",
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
