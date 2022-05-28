0a4756e0954fbd112c91e8da2dcc887a446d03a9
ersen <70ozturkersen@gmail.com>
2022-05-28 17:56:56 +0300

    ProductList page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+// import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-      <Home />
+      {/* <Home /> */}
+      <ProductList/>
     </div>
   );
 }
