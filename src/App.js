39d07e205e753943e2a70c0eeb3f5628ae77ecdb
ersen <70ozturkersen@gmail.com>
2022-05-29 09:35:19 +0300

    Cart.jsx page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Login from './pages/Login';
+import Cart from './pages/Cart';
+// import Login from './pages/Login';
 // import Register from './pages/Register';
 // import SingleProductPage from './pages/SingleProductPage';
 // import ProductList from './pages/ProductList';
 // import Home from './pages/Home';
+
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
       {/* <ProductList/> */}
       {/* <SingleProductPage /> */}
       {/* <Register/> */}
-      <Login />
+      {/* <Login /> */}
+      <Cart />
     </div>
   );
 }
